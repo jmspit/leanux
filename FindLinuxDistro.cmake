51bf98c03dd9e3106bea8c42fe68c1dadcdf2ac5
Jan-Marten Spit <spitjm@xs4all.nl>
2018-04-06 22:03:10 +0200

    switch to systemd on opensuse

--- FindLinuxDistro.cmake
+++ FindLinuxDistro.cmake
@@ -76,9 +76,9 @@ if ( EXISTS "/etc/os-release" )
       OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tools/lard/opensuse/init.d/lard ${CMAKE_CURRENT_BINARY_DIR}/init.d/lard @ONLY)
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tools/lard/debian/lard.service ${CMAKE_CURRENT_BINARY_DIR}/lard.service @ONLY)
     install(FILES ${CMAKE_CURRENT_BINARY_DIR}/lard.conf DESTINATION "/etc/lard" COMPONENT lard)
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/init.d/lard DESTINATION "/etc/init.d" COMPONENT lard)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/lard.service DESTINATION "/etc/systemd/system" COMPONENT lard)
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/tools/lard/opensuse/postinstall.sh" ${CMAKE_CURRENT_BINARY_DIR}/postinstall.sh @ONLY)
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/tools/lard/opensuse/preuninstall.sh" ${CMAKE_CURRENT_BINARY_DIR}/preuninstall.sh @ONLY)
     set( CPACK_RPM_lard_POST_INSTALL_SCRIPT_FILE "${CMAKE_CURRENT_BINARY_DIR}/postinstall.sh" )
--- tools/lard/opensuse/postinstall.sh
+++ tools/lard/opensuse/postinstall.sh
@@ -1,16 +1,23 @@
 # create user and group @LARD_USER@:
-/usr/bin/getent group @LARD_USER@ > /dev/null || /usr/sbin/groupadd -r @LARD_USER@
-/usr/bin/getent passwd @LARD_USER@ > /dev/null || /usr/sbin/useradd -r -d /bin/lard -s /sbin/nologin -g @LARD_USER@ @LARD_USER@
+getent group @LARD_USER@ > /dev/null || groupadd -r @LARD_USER@
+getent passwd @LARD_USER@ > /dev/null || useradd -r -M -s /sbin/nologin -g @LARD_USER@ @LARD_USER@
 
 # set permission on lard init script
 /usr/bin/chmod 700 @LARD_SYSVINIT_FILE@
 
 # set permission on lard config file
-/usr/bin/chmod 660 @LARD_SYSCONF_FILE@
+chmod 660 @LARD_SYSCONF_FILE@
 
-# set ownership on lard config dir
-/usr/bin/chown -R root:@LARD_USER@ /etc/lard
+# setup lard database dir
+mkdir -p /var/lib/lard
+touch /var/lib/lard/lard.db
+chown -R @LARD_USER@:@LARD_USER@ /var/lib/lard
+chmod 770 /var/lib/lard
+chmod 660 /var/lib/lard/lard.db
+
+# reload systemctl daemon status
+systemctl daemon-reload
 
 # some info to the installing user
-/bin/echo "start lard on boot: /usr/sbin/chkconfig --add lard"
-/bin/echo "start lard now: /usr/sbin/service lard start"
+echo "start lard on boot: systemctl enable lard"
+echo "start lard now: systemctl start lard"

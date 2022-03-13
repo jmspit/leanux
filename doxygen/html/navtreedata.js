63a95d00c2fb85110dcba69368555cd20f369b4d
jmspit <jmspit@users.noreply.github.com>
2022-03-13 22:15:03 +0000

    Deploying to gh-pages from @ jmspit/leanux@ad708527acca1243140af75e4aea39fb81deb85c 🚀

--- doxygen/html/navtreedata.js
+++ doxygen/html/navtreedata.js
@@ -23,12 +23,6 @@ for the JavaScript code in this file
 var NAVTREE =
 [
   [ "leanux", "index.html", [
-    [ "Introduction", "index.html#intro_section", null ],
-    [ "Using leanux", "index.html#using_section", null ],
-    [ "Namespaces", "index.html#system_section", null ],
-    [ "Building the source", "index.html#build_section", null ],
-    [ "More info", "index.html#more_info_section", null ],
-    [ "README", "d0/d30/md_README.html", null ],
     [ "Requiring root access", "d2/d30/root_access.html", null ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
@@ -69,21 +63,21 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"d1/de8/classleanux_1_1tools_1_1lmon_1_1View.html#a1c8c53fd46909428065cef90c879bcca",
-"d3/d40/classleanux_1_1sysdevice_1_1VirtualRoot.html#a0c9e2324f27847d90901535296db4d2ba858b18e604730c63c41c2d5cc51990a1",
-"d5/d01/namespaceleanux_1_1block.html#a728accf6a830322bce7171e5d88a1d11",
-"d6/d19/classleanux_1_1sysdevice_1_1BlockPartition.html#a5ec500c08ace49d1491c1b0192b736ea",
-"d7/d6a/classleanux_1_1tools_1_1lmon_1_1LardHistory.html#a41f72e91cc413643ffc0c516971760db",
-"d8/daf/classleanux_1_1persist_1_1Database.html#a24f88f33e32348bc419ec32f0880b114",
-"d9/d3a/lblk_8cpp.html#a73d92bf0717d3dece131f9cf718de164",
-"d9/db2/block_8hpp.html#ab20a8824019f463ba684c0cfadfd7117",
-"da/da6/lar__schema_8cpp.html#af1d9ed71437fe1246062c7d5dcde6eda",
-"db/d5e/classleanux_1_1sysdevice_1_1USBDevice.html#aea05b28dc39c8d625bed737ef1777eb1",
-"dc/d7e/classleanux_1_1tools_1_1lmon_1_1Header.html#a4371f35535c3f1ab38f15ab859564535",
-"dd/d47/classleanux_1_1sysdevice_1_1RamDiskDevice.html#a0c9e2324f27847d90901535296db4d2ba6946e387b6f7c448ae47f5df10bdf9a1",
-"dd/dff/classleanux_1_1sysdevice_1_1BusDevice.html#a0c9e2324f27847d90901535296db4d2bae9956f97d870afad1c62b95b9827b819",
-"de/dd2/classleanux_1_1sysdevice_1_1LoopbackDevice.html#a0c9e2324f27847d90901535296db4d2ba33e67a61818fcff4915881bf129b55f3",
-"df/dcc/classleanux_1_1tools_1_1lmon_1_1NetView.html#ac660a760ae4bbb3b9a6762ed41f765dd"
+"d1/de8/classleanux_1_1tools_1_1lmon_1_1View.html#a1cac12298a9e631c6ad33bc3e8695cea",
+"d3/d40/classleanux_1_1sysdevice_1_1VirtualRoot.html#a0c9e2324f27847d90901535296db4d2ba898f7164f24f64efb51b8b96d7ec3839",
+"d5/d01/namespaceleanux_1_1block.html#a739ff0dc49c058d7d888adf7aac6840d",
+"d6/d19/classleanux_1_1sysdevice_1_1BlockPartition.html#a66d61b5a78be267835e09b8d9e4c405f",
+"d7/d6a/classleanux_1_1tools_1_1lmon_1_1LardHistory.html#a4208f80a039f6abbc8faf6df1bbf4d85",
+"d8/daf/classleanux_1_1persist_1_1Database.html#a295f9d0b2e58507a2e0a744d85931e99",
+"d9/d3a/lblk_8cpp.html#a7e4a52393fd43c98af0e5e16603b54df",
+"d9/db2/block_8hpp.html#ac08a21002b48cb1bbfaf8d77b4aac5f6",
+"da/da6/lar__schema_8cpp.html#af293f8b5b80c0189b1dc1904fe94d9bf",
+"db/d5e/classleanux_1_1sysdevice_1_1USBDevice.html#af77fda9c4f8ac98eab9afe80d5e9b1d2",
+"dc/d7e/classleanux_1_1tools_1_1lmon_1_1Header.html#a5894ed0fe491ea3bf2a97f7017c5cf6e",
+"dd/d47/classleanux_1_1sysdevice_1_1RamDiskDevice.html#a0c9e2324f27847d90901535296db4d2ba6dd8a43a2da59f5d1cb2b5e4073b3ac6",
+"dd/dff/classleanux_1_1sysdevice_1_1BusDevice.html#a0c9e2324f27847d90901535296db4d2baee181d6e6f3fe5ec2ecbe62128cd23af",
+"de/dd2/classleanux_1_1sysdevice_1_1LoopbackDevice.html#a0c9e2324f27847d90901535296db4d2ba397d82f29c3dc0bd4f0ba4dae6115127",
+"df/dcc/classleanux_1_1tools_1_1lmon_1_1NetView.html#acbc383deda34282bcdb725b16ad3d9ee"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
